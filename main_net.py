cc020a75a4b827b06feccf010725babc2f633a98
itamar gruber <itamar.gru@gmail.com>
2020-01-22 22:02:06 +0200

    updated the main_net.py with the meeting's notes

--- main_net.py
+++ main_net.py
@@ -2,8 +2,17 @@
 import sys, getopt
 
 def main(argv):
-    # TODO: add a way to parse argument, serch google for:
+    # TODO: add a way to parse argument, search google for:
     #  opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+
+    # init log file
+    # set seeds (for random functions)
+    # device: cuda, cpu
+    # create the network
+    # if task == "train"/"test"
+    #   init "test"/"train"
+    #   run "test"/"train"
+    # result + plots
     pass
 
 
