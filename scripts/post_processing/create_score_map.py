1aa8b2dd7f0956ad150b4c4810de1a7fdc7973ee
dekel <34662967+dekool@users.noreply.github.com>
2021-12-12 12:01:31 +0200

    Create create_score_map.py

---
+++ scripts/post_processing/create_score_map.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import numpy as np
+import argparse
+import pickle
+import csv
+import os
+from PIL import Image
+
+def extract_row_and_col(img_name):
+    row = img_name.split("_")[2]
+    col = img_name.split("_")[3].split(".")[0]
+    return row, col
+
+def create_score_map(input_path, output_path):
+    with open(input_path, "rb") as results_file:
+        results_data = pickle.load(results_file)
+
+    with open(os.path.join(output_path, "real_slide_scores_locations.csv"), mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        max_row = 0
+        max_col = 0
+        for img_name in results_data['areas_per_image_air_filt']:
+            row, col = extract_row_and_col(img_name)
+            max_row = max(int(row), max_row)
+            max_col = max(int(col), max_col)
+        slide_matrix = np.zeros((max_row, max_col))
+        slide_matrix = slide_matrix - 1
+        for img_name in results_data['areas_per_image_air_filt']:
+            row, col = extract_row_and_col(img_name)
+            img_areas = results_data['areas_per_image_air_filt'][img_name]
+            if img_areas[0] + img_areas[1] == 0:
+                img_pred_score = 0
+            else:
+                img_pred_score = img_areas[0] / (img_areas[0] + img_areas[1])
+            slide_matrix[int(row)-1, int(col)-1] = img_pred_score
+        for i in range(slide_matrix.shape[0]):
+            new_row = []
+            for j in range(slide_matrix.shape[1]):
+                new_row.append(slide_matrix[i, j])
+            csv_writer.writerow(new_row)
+            
+    img = Image.fromarray(np.uint8(slide_matrix * 255) , 'L')
+    img.save(os.path.join(output_path, "real_slide_location_map.png"), format="png")
+        
+def main():
+    parser = argparse.ArgumentParser(description='This script is ...'
+                                    , formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("--input", "-i", default="./input",
+                        help="Path to the directory of the result pickle file. default='./input'")
+    parser.add_argument("--output", "-o", default="./output",
+                        help="Directory name where the result csv file and image will be saved. default='./output'")
+
+    args = parser.parse_args()
+    create_score_map(args.input, args.output)
+    
+    return
+
+
+if __name__ == "__main__":
+    main()
